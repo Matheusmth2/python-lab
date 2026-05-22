20dfd47302bd28b822cb4f3e971501cc11488306
Matheus Medeiros <matheusmed03@gmail.com>
2026-05-22 12:56:55 -0300

    Calcular as raizes de uma equacao do segundo grau

--- testando.py
+++ testando.py
@@ -1,7 +1,16 @@
-# Este código calcula o Índice de Massa Corporal (IMC) com base na massa e altura fornecidas.
+# Este código calcula as raízes de uma equação quadrática usando a fórmula de Bhaskara.
 
-mass = 70  # massa em kg
-height = 1.75 # altura em metros
-bmi = mass / (height ** 2) # fórmula para calcular o IMC
+# O usuário é solicitado a inserir o valor de a, que é o coeficiente do termo quadrático na equação.
+a = int(input("Digite o valor de a: "))
+# O usuário é solicitado a inserir o valor de b, que é o coeficiente do termo linear na equação.
+b = int(input("Digite o valor de b: "))
+# O usuário é solicitado a inserir o valor de c, que é o coeficiente do termo constante na equação.
+c = int(input("Digite o valor de c: "))
 
-print("O Índice de Massa Corporal (IMC) é:", bmi)
+# Calcula a primeira raiz usando a fórmula de Bhaskara e imprime o resultado.
+x1 = (-b + (b**2 - 4*a*c) ** (1/2)) / (2*a)
+print("O valor da primeira raiz é:", x1)
+
+# Calcula a segunda raiz usando a fórmula de Bhaskara e imprime o resultado.
+x2 = (-b - (b**2 - 4*a*c) ** (1/2)) / (2*a)
+print("O valor da segunda raiz é:", x2)
