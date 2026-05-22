39b3149cc101ca1e0d40cf18a0dea043f4210574
Matheus Medeiros <matheusmed03@gmail.com>
2026-05-22 12:39:32 -0300

    Meu eu do futuro

--- testando.py
+++ testando.py
@@ -1,2 +1,10 @@
-# Para começar sempre o famoso Hello World
-print("Hello World")
+# Today's date
+print("05-22-2026")
+# How am I feeling right now?
+print("Very happy!😁")
+# What do I hope to achieve by learning to program?
+print("To be a great cybersecurity")
+# A message for my older, wiser, and more programmer self.
+print("It was worth it.")
+# My favorite emoji
+print("😊")
