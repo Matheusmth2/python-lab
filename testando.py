2b30801edbf79619cbe45d8fbb35eecf6ebeb16c
Matheus Medeiros <matheusmed03@gmail.com>
2026-05-26 15:02:48 -0300

    Outra meneira de localizar uma substring

--- testando.py
+++ testando.py
@@ -1,3 +1,3 @@
 # Localizando uma substring dentro de uma string usando o método index()
-tshah_index = "tsnow, tshah, bmoreno - updated".index("tshah")
-print(tshah_index)
+tshah_index = "tsnow, tshah, bmoreno - updated"
+print(tshah_index.index("tshah"))
