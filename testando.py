b7e0779b0c44a072a9c1e4b9d64723880fe7f3dd
Matheus Medeiros <matheusmed03@gmail.com>
2026-05-26 10:55:31 -0300

    Convertendo outros tipos de dados, para string

--- testando.py
+++ testando.py
@@ -1,3 +1,15 @@
-# Convertendo um número inteiro para uma string
+# Convertendo um valor booleano para uma string
+new_string = str(True)
+print(type(new_string))
+
+# Convertendo um valor inteiro para uma string
 new_string = str(123)
+print(type(new_string))
+
+# Convertendo um valor float para uma string
+new_string = str(3.14)
+print(type(new_string))
+
+# Convertendo um valor None para uma string
+new_string = str(None)
 print(type(new_string))
