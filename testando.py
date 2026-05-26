b7c7758481093f99f1019dc2bafef54ec0bdfa73
Matheus Medeiros <matheusmed03@gmail.com>
2026-05-26 16:25:07 -0300

    Utilizando o append para adicionar no final da lista

--- testando.py
+++ testando.py
@@ -22,3 +22,12 @@ print(my_list)  # Imprime a lista atualizada
 # Utilizando o método de remoção para remover um elemento específico da lista
 my_list.remove("laranja")  # Remove "laranja" da lista
 print(my_list)  # Imprime a lista atualizada
+
+# Adiciona uma entrada ao final de uma lista. O único parâmetro é o elemento que deseja adicionar ao final da lista.
+username_list = ["bmoreno", "wjaffrey", "tshah", "sgilmore"]
+print("Before appending an element:", username_list)
+username_list.append("btang")
+print("After appending an element:", username_list)
+# Se preferir poderia colocar uma variavel para o elemento a ser adicionado, como por exemplo:
+new_username = "btang"
+username_list.append(new_username)
