c5c6851f4c4158bcb8ca96afe3ebf8b5c4bdd083
Matheus Medeiros <matheusmed03@gmail.com>
2026-05-26 15:22:56 -0300

    Pesquisando por indice em uma lista, concatenando duas listas, substitiundo, adicionando e removendo intens de uma lista

--- testando.py
+++ testando.py
@@ -1,3 +1,24 @@
-# Localizando uma substring dentro de uma string usando o método index()
-tshah_index = "tsnow, tshah, bmoreno - updated"
-print(tshah_index.index("tshah"))
+# Acessando elementos de uma lista usando índices
+my_list = ["banana", "maçã", "laranja", "uva"]
+print(my_list[0])  # Acessa o primeiro elemento da lista
+print(my_list[1])  # Acessa o segundo elemento da lista
+print(my_list[2])  # Acessa o terceiro elemento da lista
+print(my_list[3])  # Acessa o quarto elemento da lista
+
+# Concatenando listas
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+print(list1 + list2)  # Concatena as duas listas
+
+# Diferente de strings, as listas são mutáveis, ou seja, seus elementos podem ser alterados após a criação da lista.
+# Mundando o valor de um elemento da lista
+my_list[1] = 7  # Altera o segundo elemento da lista
+print(my_list)  # Imprime a lista atualizada
+
+# Utilizando o método de inserção para adicionar um elemento em uma posição específica da lista
+my_list.insert(2, "abacaxi")  # Insere "abacaxi" na posição 2 da lista
+print(my_list)  # Imprime a lista atualizada
+
+# Utilizando o método de remoção para remover um elemento específico da lista
+my_list.remove("laranja")  # Remove "laranja" da lista
+print(my_list)  # Imprime a lista atualizada
