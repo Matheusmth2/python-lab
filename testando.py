bfa7698adcfc500bd708286dcd5c34bc29ae1b12
Matheus Medeiros <matheusmed03@gmail.com>
2026-05-26 10:58:30 -0300

    Comprimento de uma string

--- testando.py
+++ testando.py
@@ -1,15 +1,2 @@
-# Convertendo um valor booleano para uma string
-new_string = str(True)
-print(type(new_string))
-
-# Convertendo um valor inteiro para uma string
-new_string = str(123)
-print(type(new_string))
-
-# Convertendo um valor float para uma string
-new_string = str(3.14)
-print(type(new_string))
-
-# Convertendo um valor None para uma string
-new_string = str(None)
-print(type(new_string))
+# Este é um script simples em Python que imprime o comprimento (length) da string "Hello World!".
+print(len("Hello World!"))
