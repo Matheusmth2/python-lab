f7c0cd6c0655b6be7ca40f4e84d38a24dc0a510f
Matheus Medeiros <matheusmed03@gmail.com>
2026-05-26 11:13:25 -0300

    utilizando indices

--- testando.py
+++ testando.py
@@ -1,5 +1,18 @@
-# Utilizando o método upper() para converter "Hello, World!" em letras maiúsculas
-print("Hello, World!".upper())
+# Utilizando índices para acessar caracteres em uma string
+# Em Python, as strings são indexadas a partir do zero. Isso significa que o primeiro caractere de uma string tem o índice 0, o segundo caractere tem o índice 1, e assim por diante. Você pode usar colchetes [] para acessar caracteres específicos em uma string.
+# Por exemplo, se você tiver a string "Hello", os índices dos caracteres seriam:
+# H -> índice 0 e e -> índice 1 e l -> índice 2 e l -> índice 3 e o -> índice 4
+# Podemos acessar cada caractere usando seu índice correspondente. Aqui estão alguns exemplos:
+print("Hello"[0])
 
-# Utilizando o método lower() para converter "Hello, World!" em letras minúsculas
-print("Hello, World!".lower())
+print("Hello"[1])
+
+print("Hello"[2])
+
+print("Hello"[3])
+
+print("Hello"[4])
+
+print("Hello"[0:3])
+
+print("Hello"[1:4])
